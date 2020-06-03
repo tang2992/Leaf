947c9b642726ae3deb323d51ad36331c6b6de825
tangfk <tang2992@126.com>
2020-06-03 23:12:56 +0800

    扩展循环计数和按规则切片的实现方式

---
+++ leaf-core/src/main/java/com/sankuai/inf/leaf/segment/dao/impl/IDShortAllocDaoImpl.java
@@ -0,0 +1,75 @@
+package com.sankuai.inf.leaf.segment.dao.impl;
+
+import com.sankuai.inf.leaf.segment.dao.IDAllocDao;
+import com.sankuai.inf.leaf.segment.dao.IDAllocMapper;
+import com.sankuai.inf.leaf.segment.dao.IDShortAllocMapper;
+import com.sankuai.inf.leaf.segment.model.LeafAlloc;
+import org.apache.ibatis.mapping.Environment;
+import org.apache.ibatis.session.Configuration;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.apache.ibatis.transaction.TransactionFactory;
+import org.apache.ibatis.transaction.jdbc.JdbcTransactionFactory;
+
+import javax.sql.DataSource;
+import java.util.List;
+
+public class IDShortAllocDaoImpl implements IDAllocDao {
+
+    SqlSessionFactory sqlSessionFactory;
+
+    public IDShortAllocDaoImpl(DataSource dataSource) {
+        TransactionFactory transactionFactory = new JdbcTransactionFactory();
+        Environment environment = new Environment("development", transactionFactory, dataSource);
+        Configuration configuration = new Configuration(environment);
+        configuration.addMapper(IDShortAllocMapper.class);
+        sqlSessionFactory = new SqlSessionFactoryBuilder().build(configuration);
+    }
+
+    @Override
+    public List<LeafAlloc> getAllLeafAllocs() {
+        SqlSession sqlSession = sqlSessionFactory.openSession(false);
+        try {
+            return sqlSession.selectList("com.sankuai.inf.leaf.segment.dao.IDShortAllocMapper.getAllLeafAllocs");
+        } finally {
+            sqlSession.close();
+        }
+    }
+
+    @Override
+    public LeafAlloc updateMaxIdAndGetLeafAlloc(String tag) {
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        try {
+            sqlSession.update("com.sankuai.inf.leaf.segment.dao.IDShortAllocMapper.updateMaxId", tag);
+            LeafAlloc result = sqlSession.selectOne("com.sankuai.inf.leaf.segment.dao.IDShortAllocMapper.getLeafAlloc", tag);
+            sqlSession.commit();
+            return result;
+        } finally {
+            sqlSession.close();
+        }
+    }
+
+    @Override
+    public LeafAlloc updateMaxIdByCustomStepAndGetLeafAlloc(LeafAlloc leafAlloc) {
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        try {
+            sqlSession.update("com.sankuai.inf.leaf.segment.dao.IDShortAllocMapper.updateMaxIdByCustomStep", leafAlloc);
+            LeafAlloc result = sqlSession.selectOne("com.sankuai.inf.leaf.segment.dao.IDShortAllocMapper.getLeafAlloc", leafAlloc.getKey());
+            sqlSession.commit();
+            return result;
+        } finally {
+            sqlSession.close();
+        }
+    }
+
+    @Override
+    public List<String> getAllTags() {
+        SqlSession sqlSession = sqlSessionFactory.openSession(false);
+        try {
+            return sqlSession.selectList("com.sankuai.inf.leaf.segment.dao.IDShortAllocMapper.getAllTags");
+        } finally {
+            sqlSession.close();
+        }
+    }
+}
---
+++ leaf-core/src/test/java/com/sankuai/inf/leaf/segment/IDShortGenServiceTest.java
@@ -0,0 +1,65 @@
+package com.sankuai.inf.leaf.segment;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import com.sankuai.inf.leaf.IDGen;
+import com.sankuai.inf.leaf.common.PropertyFactory;
+import com.sankuai.inf.leaf.common.Result;
+import com.sankuai.inf.leaf.segment.dao.IDAllocDao;
+import com.sankuai.inf.leaf.segment.dao.impl.IDAllocDaoImpl;
+import com.sankuai.inf.leaf.segment.dao.impl.IDShortAllocDaoImpl;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class IDShortGenServiceTest {
+    IDGen idGen;
+    DruidDataSource dataSource;
+    @Before
+    public void before() throws IOException, SQLException {
+        // Load Db Config
+        Properties properties = PropertyFactory.getProperties();
+
+        // Config dataSource
+        dataSource = new DruidDataSource();
+        dataSource.setUrl(properties.getProperty("leaf.jdbc.url"));
+        dataSource.setUsername(properties.getProperty("leaf.jdbc.username"));
+        dataSource.setPassword(properties.getProperty("leaf.jdbc.password"));
+        dataSource.init();
+
+        // Config Dao
+        IDAllocDao dao = new IDShortAllocDaoImpl(dataSource);
+
+        // Config ID Gen
+        idGen = new SegmentIDGenImpl();
+        ((SegmentIDGenImpl) idGen).setDao(dao);
+        idGen.init();
+    }
+    @Test
+    public void testGetId() {
+        for (int i = 0; i < 100; ++i) {
+            Result r = idGen.get("leaf-segment-short-test");
+            System.out.println(r);
+        }
+    }
+    @Test
+    public void testGetId2() {
+        for (int i = 0; i < 100; ++i) {
+            Result r = idGen.get("leaf-segment-test");
+            System.out.println(r);
+        }
+    }
+    @After
+    public void after() {
+       dataSource.close();
+    }
+
+    @Test
+    public void bit() {
+        long sequenceMask = ~(-1L << 12);
+        System.out.println(sequenceMask);
+    }
+}
